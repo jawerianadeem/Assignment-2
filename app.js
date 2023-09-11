f458ed5a3a5a6dad835eedd9d461d8e4d6a0c4d9
jawerianadeem <143826078+jawerianadeem@users.noreply.github.com>
2023-09-12 00:12:13 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,138 @@
+// Chapter 6 (Math Expression II)
+// Q1
+var num= x++;
+// Q2
+var x=100;
+var res = --x;
+console.log(res);
+// Q3
+var x=50;
+var y= x++;
+var res= 50;
+console.log(res);
+// Q4
+var y = 50;
+var z = --y;
+var res= 49;
+console.log(res);
+// Q5
+var num1=2;
+var num2=--num1;
+var num3= 2;
+// Q6
+var j=5;
+var num=j++;
+var res=5;
+
+// Q7
+var a=9;
+var num=++a;
+alert("10");
+
+
+
+// Chapter 7 (Math Expression III)
+// Q1
+var calculatedNum = 2 + (2 * 6);
+var res= 14;
+console.log(res);
+// Q2
+var calculatedNum = (2 + 2) * 6;
+var res= 24 ;
+console.log(res);
+// Q3
+var calculatedNum = (2 + 2) * (4 + 2);
+var res= 24 ;
+console.log(res);
+// Q4
+var calculatedNum = ((2 + 2) * 4) + 2;
+var res= 18 ;
+console.log(calculatedNum);
+// Q5
+
+var num=( 2 + 2) * (4 + 10);
+console.log(num);
+// Q6
+var calculatedNum=2+(2*4)+10;
+console.log(calculatedNum);
+// Q7
+var value=4 / 2 * 4-3;
+console.log(value);
+
+
+
+
+// Chapter 8 (Concatenating Text Strings)
+// Q1
+var num = "2" + "2";
+console.log(num);
+// Q2
+var message = ("Hello," + "Dolly");
+alert(message);
+// Q3
+var num=("33" + 3);
+alert(num);
+// Q4
+var value=("Pakistan"+"Zindabad");
+alert(value);
+// Q5
+var pro = "haha" + 11;
+// Q6
+var word1 ="you"+"are";
+var word2 =word1+"nice person";
+
+
+
+// Chapter 9 (Prompts)
+// Q1
+var userFirstName=prompt("Enter user first name");
+// Q2
+var country=prompt("country");
+if (country==="china")
+// Q3
+var yourName = prompt("Enter Your Name");
+// Q4
+var line =prompt("Hey user!")
+// Q5
+
+// Q6
+var message=prompt("Hello!");
+if (message==="hello!"){
+    alert("Hey!");
+}
+else if(message==="hello!"){
+    alert("Sorry");
+}
+
+
+
+
+// Chapter 10 (if statments)
+// Q1
+var city = "Karachi";
+if (city === "Karachi") {
+console.log("The City OF Lights");}
+// Q2
+var value="A-Z"
+if (value === "y"){
+    console.log("z");
+}
+// Q3
+var value=10010;
+if (value===10010){
+    alert("karachi");
+}
+else {alert ("Please write correct city");}
+
+
+
+// Chapter 11 (Comparison Operators)
+// Q1
+
+
+
+
+
+
+
+
